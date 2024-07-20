dd5f61cb84ddfe3ba1784a2470446d5c51457ca6
RajeshKundara <rajeshkundara037@gmail.com>
2024-07-20 11:10:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,411 @@
+<?php
+$domain =  "https://pokkawin.com";
+$host =  $domain."?c=";
+$title = 'Pokkawin: Situs Slot Gacor Maxwin Gampang JP Bonus New Member 100%';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title><?=$title?></title> 
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <meta name="description"
+        content="Daftar Pokkawin akun link slot gacor maxwin hari ini gampang menang jp 2023. Slot88 dengan minimal slot depo 10k, slot deposit 10 ribu, slot deposit 10rb!">
+    <meta name="keywords"
+        content="Pokkawin, slot, slot88, slot gacor, slot online, slot 2023, link slot, daftar slot, slot 10k, slot 10 ribu, slot 10rb, slot 10000, slot gacor maxwin, slot gacor 2023, link slot gacor, daftar slot gacor, slot gampang jp, judi slot online, slot depo 10k, slot deposit 10 ribu, slot deposit 10rb, slot deposit 10000, slot gacor hari ini, slot gacor gampang menang">
+    <meta name="author" content="Pokkawin">
+    <meta content="website" property="og:type">
+    <meta content="Pokkawin: Situs Slot Gacor Maxwin Gampang JP Min Depo 10k" property="og:title">
+    <meta
+        content="Daftar Pokkawin akun link slot gacor maxwin hari ini gampang menang jp 2023. Slot88 dengan minimal slot depo 10k, slot deposit 10 ribu, slot deposit 10rb!"
+        property="og:description">
+    <meta content="<?=$domain?>" property="og:url">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">  
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	<?php $uri=(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER['HTTP_HOST']; 
+	header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+	header("Pragma: no-cache"); //HTTP 1.0
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+	//or, if you DO want a file to cache, use:
+	header("Cache-Control: max-age=2592000"); 
+	?>
+	<meta http-equiv="Cache-control" content="public">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+
+	<meta name="google-site-verification" content="URRR3cfioEEpF3NSHfWUuE-W8g3KVjrVT3wmjfhIySM">    
+	<meta name="twitter:card" content="summary_large_image"/>    
+	<meta name="twitter:description" content="ECarJax offers reliable and on-time Airport transportation, ✈ Options for Executive Transportation, Chauffeured Luxury Vehicles, JAX Airport Transfers, Airport Limo, Special Events Car Service, Sedans & SUV's services in Jacksonville."/>    
+	<meta name="twitter:card" content="summary_large_image"/>    
+	<meta name="twitter:description" content="ECarJax offers reliable and on-time Airport transportation, ✈ Options for Executive Transportation, Chauffeured Luxury Vehicles, JAX Airport Transfers, Airport Limo, Special Events Car Service, Sedans & SUV's services in Jacksonville."/>  
+	<meta name="description" content="Prom Limo Services, Graduation Limo Service, and Homecoming Transportation by ECarJax Jacksonville Florida and Ponte Vedra Beach Florida. Book online or call us today at 904.537.5820." />
+	<meta name="keywords" content="Tesla, transportation, graduation black car service, graduation limo service, prom black car service, prom limo service" />
+
+
+	<meta property="fb:app_id" content="2260214710921778" />
+	<meta property="og:title" content="<?=$title?>" />
+	<meta property="og:image" content="<?=$uri."images/banner.webp"?>" />
+	<meta property="og:url" content="<?=$uri."/".$_SERVER['REQUEST_URI']?>" />
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="ECarJax offers reliable and on-time Airport transportation, ✈ Options for Executive Transportation, Chauffeured Luxury Vehicles, JAX Airport Transfers, Airport Limo, Special Events Car Service, Sedans & SUV's services in Jacksonville." />
+	
+ 	<meta http-equiv="Cache-control" content="public">
+ 
+    <meta name="generator" content="JDPC Global CMS 2.0.0.76">
+    <link href="<?=$dir?>media/testimonials/css/icore_testimonials.css" media="all" type="text/css" rel="stylesheet" />
+    
+    
+	<meta name="apple-mobile-web-app-title" content="ECarJax">    
+	<meta name="apple-mobile-web-app-capable" content="yes">    
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<style>
+		.fs{
+			text-align: center;
+			font-family: "YACgEcYqQ-A 0", "_fb_", "auto";font-weight: 400;font-style: normal;
+		}
+		.header{
+			color: rgb(255,255,255) !important;
+			font-size: 40px;
+			text-transform: uppercase;
+			filter: drop-shadow(rgba(199, 7, 223, 0.95) 0px 0px 0.4225px) drop-shadow(rgba(202, 35, 223, 0.75) 0px 0px 2.1125px) drop-shadow(rgba(202, 35, 223, 0.44) 0px 0px 6.3375px);
+		}
+		.bg-btn{
+			background:url(images/bg_btn.png);
+			background-size:contain;
+			background-repeat:no-repeat;
+			height:auto;
+			width:100%;
+			background-position: center center;   
+		}
+		
+		.fonts-country{
+			color:#fff;
+		}
+		.btn-country{
+			width: 100%;
+			border: 3px solid #D8DEFE;
+			border-radius: 50px;
+			position: relative;
+			background-image: linear-gradient(#0C0F4A, #0A11CD);
+			box-shadow: #0A11CD 0 -1px 17px 11px, inset #0A11CD 0 -1px 9px, #0A11CD 0 12px 12px;
+			text-transform:uppercase;
+		}
+		.vertical-center{
+			position: absolute;
+		  top: 50%;
+		  left: 50%;
+		  transform: translate(-50%, -50%);
+		}
+		.about-text{
+			color: rgb(255,255,255) !important;
+			font-size: 30px;
+			text-transform: uppercase;
+			filter: drop-shadow(rgba(199, 7, 223, 0.95) 0px 0px 0.4225px) drop-shadow(rgba(202, 35, 223, 0.75) 0px 0px 2.1125px) drop-shadow(rgba(202, 35, 223, 0.44) 0px 0px 6.3375px);
+		}
+		.footer-wrap {
+			display: grid;
+			grid-template-columns: 30% 70%;
+			padding: 10px;
+		}
+		.contact-details {
+			display: grid;
+			margin-left: 10px;
+			align-self: center;
+		}
+		.p-1 {
+			color: #ffffff;
+			padding: 0 !important;
+		}
+		.p-2 {
+			color: #ffffff;
+			padding: 0 !important;
+		}
+		.footer-link-copyright {
+			background: linear-gradient(to right, #000 0%, #4a1890 50%, #000 100%);
+				background-size: auto;
+			background-size: 100% 100%;
+			padding-top: 10px;
+			padding-bottom: 10px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		.list-inline {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin-bottom: 0;
+			padding-left: 0;
+			list-style: none;
+		}
+		.footer-right a {
+			color: #ffffff;
+			border-right: 1px solid #ffffff;
+			padding-left: 10px;
+			padding-right: 10px;
+		}
+		.copyright-line:last-child {
+			border-right: none;
+		}
+		.footer-copyright {
+			padding-top: 20px;
+			padding-bottom: 20px;
+			color: #6d6d6d;
+		}
+	</style><!--.bg-btn:after {
+		  padding-top: 25%;
+		  /* this llama image is 800x600 so set the padding top % to match 600/800 = .75 */
+		  display: block;
+		  content: '';
+		}-->
+	
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="fontawesome/css/fontawesome.min.css" rel="stylesheet">
+	<link href="fontawesome/css/solid.min.css" rel="stylesheet">
+</head>
+<body style="margin:0px;">
+	<div style="width:100%; height:100%; overflow-y:scroll;overflow-x:hidden;background:url(images/bg.webp)">
+		<div>
+			<img src="images/logo.png" alt="" style="width:250px;margin:auto;display: flex;"/>
+		</div>
+		<div>
+			<img src="images/banner.webp" alt="" style="width:100%;"/>
+		</div>
+		<div class="p-3">
+			<h2 class="fs fs-2 header">SIlakan pilih server slot yang anda inginkan</h2>
+		</div>
+		<div class="row mb-3">
+			<div class="col-1"></div>
+			<div class="col-5 d-flex justify-content-start">
+				<div style="max-width:200px">
+					<img src="images/flag_indonesia.webp" alt="" class="img-fluid" style=""/>
+				</div>
+				<div style="max-width:400px;margin-left:-30%">
+					<img src="images/server.webp" alt="" class="img-fluid" style=""/>
+				</div>
+			</div>
+			<div class="col-5 d-flex justify-content-start">
+				<div style="max-width:200px">
+					<img src="images/flag_thailand.webp" alt="" class="img-fluid" style=""/>
+				</div>
+				<div style="max-width:400px;margin-left:-30%">
+					<img src="images/server.webp" alt="" class="img-fluid" style=""/>
+				</div>
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row text-center mb-3">
+			<div class="col-1"></div>
+			<div class="col-5 p-3 d-flex justify-content-start">
+				
+					<button class="p-3 text-white btn-country" >
+						<a href="<?=$host?>Indonesia" class=" text-white text-decoration-none" >
+							Indonesia
+						</a>
+						<i class="fa-solid fa-angles-right"></i>
+					</button>
+			</div>
+			<div class="col-5 p-3 d-flex justify-content-start">
+				
+					<button class="p-3 text-white btn-country" >
+					<a href="<?=$host?>Thailand" class=" text-white text-decoration-none" >
+						Thailand
+						</a>
+						<i class="fa-solid fa-angles-right"></i>
+					</button>
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row mb-3">
+			<div class="col-1"></div>
+			<div class="col-5 d-flex justify-content-start">
+				<div style="max-width:200px">
+					<img src="images/flag_malaysia.webp" alt="" class="img-fluid" style=""/>
+				</div>
+				<div style="max-width:400px;margin-left:-30%">
+					<img src="images/server.webp" alt="" class="img-fluid" style=""/>
+				</div>
+			</div>
+			<div class="col-5 d-flex justify-content-start">
+				<div style="max-width:200px">
+					<img src="images/flag_china.webp" alt="" class="img-fluid" style=""/>
+				</div>
+				<div style="max-width:400px;margin-left:-30%">
+					<img src="images/server.webp" alt="" class="img-fluid" style=""/>
+				</div>
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row text-center mb-3">
+			<div class="col-1"></div>
+			<div class="col-5 p-3 d-flex justify-content-start">
+				
+					<button class="p-3 text-white btn-country" >
+						<a href="<?=$host?>Malaysia" class=" text-white text-decoration-none" >
+							Malaysia
+						</a>
+						<i class="fa-solid fa-angles-right"></i>
+					</button>
+				
+			</div>
+			<div class="col-5 p-3 d-flex justify-content-start">
+				
+					<button class="p-3 text-white btn-country" >
+						<a href="<?=$host?>China" class=" text-white text-decoration-none" >
+							China
+						</a>
+						<i class="fa-solid fa-angles-right"></i>
+					</button>
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row mb-3">
+			<div class="col-1"></div>
+			<div class="col-5 d-flex justify-content-start">
+				<div style="max-width:200px">
+					<img src="images/flag_hongkong.webp" alt="" class="img-fluid" style=""/>
+				</div>
+				<div style="max-width:400px;margin-left:-30%">
+					<img src="images/server.webp" alt="" class="img-fluid" style=""/>
+				</div>
+			</div>
+			<div class="col-5 d-flex justify-content-start">
+				<div style="max-width:200px">
+					<img src="images/flag_korea.webp" alt="" class="img-fluid" style=""/>
+				</div>
+				<div style="max-width:400px;margin-left:-30%">
+					<img src="images/server.webp" alt="" class="img-fluid" style=""/>
+				</div>
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row text-center mb-3">
+			<div class="col-1"></div>
+			<div class="col-5 p-3 d-flex justify-content-start">
+				
+					<button class="p-3 text-white btn-country" >
+						<a href="<?=$host?>HONGKONG" class=" text-white text-decoration-none" >
+							HONG KONG
+						</a>
+					
+						<i class="fa-solid fa-angles-right"></i>
+					</button>
+				</a>
+			</div>
+			<div class="col-5 p-3 d-flex justify-content-start">
+				
+				<button class="p-3 text-white btn-country" >
+					<a href="<?=$host?>KOREA" class=" text-white text-decoration-none" >
+						KOREA
+					</a>
+					<i class="fa-solid fa-angles-right"></i>
+				</button>
+				
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row   bg-black">
+			<div class="col-1"></div>
+			<div class="col-10 p-3">
+				<h3 class="text-decoration-underline  about-text" >ABOUT US</h3> 
+				<p style="width: 100%; color: white;">Pokkawin adalah situs slot terpercaya di Asia Tenggara. Kami menggunakan server  berteknologi tinggi dan RTP yang tinggi di berbagai negara untuk menjadi pilihan yang terbaik untuk pelanggan kami. Kami juga memberikan kemudahan deposit bermain slot online dengan menggunakan PULSA TANPA POTONGAN dan PEMBAYARAN OTOMATIS, serta pembayaran Banking | E- Wallet. Lisensi POKKAWIN bersertifikat asli sehingga situs Pokkawin ini telah terakreditasi. 
+								Layanan CS 24/7 siap membantu member yang memiliki kendala ataupun pertanyaan.</p>
+			</div>
+			
+			<div class="col-1"></div>
+		</div>
+
+		<div class="row  bg-black p-1">
+			<div class="col-2">
+				<a href="https://direct.lc.chat/15594276/ target="_blank" class="footer-wrap text-decoration-none">
+					<img src="images/icon-contact.png" class="img-fluid">
+					<div class="contact-details">
+						<span class="p-1">LIVE CHAT</span>
+						<span class="p-2 fs-6">Let the Pokka Hero to assist you!</span>
+					</div>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="https://wa.me/+6281329527192" target="_blank" class="footer-wrap text-decoration-none">
+						<img src="images/contact-whatsapp.png" class="img-fluid">
+						<div class="contact-details">
+							<span class="p-1">WHATSAPP</span>
+							<span class="p-2">+62 81329527192</span>
+						</div>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="Https://t.me/pokkawin_official" target="_blank" class="footer-wrap text-decoration-none">
+					<img src="images/contact-telegram.png" class="img-fluid">
+					<div class="contact-details">
+						<span class="p-1">TELEGRAM</span>
+						<span class="p-2">POKKAWIN_OFFICIAL</span>
+					</div>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="mailto:pokkawin.cs01@gmail.com" target="_blank" class="footer-wrap text-decoration-none">
+					<img src="images/contact-email.png" class="img-fluid">
+					<div class="contact-details">
+						<span class="p-1">EMAIL</span>
+						<span class="p-2">POKKAWIN.CS01@GMAIL.COM</span>
+					</div>
+				</a>
+			</div>
+		</div>
+          
+		<div class="row bg-black">
+			<div class="col-12 px-1">
+				<img src="images/photo_6174584520083355643_y.jpg" alt="games plateformes" width="100%">
+			</div>
+
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<div class="footer-link-copyright">
+					<div class="footer-link">
+						<div class="container">
+							<ul class="list-inline footer-right">
+								<li class="copyright-line">
+									<a href="<?=$host?>/web/aboutus">Tentang Kami</a>
+								</li>
+								<li class="copyright-line">
+									<a href="<?=$host?>/web/contact">Hubungin Kami</a>
+								</li>
+								<li class="copyright-line">
+									<a href="<?=$host?>/web/policy">Kebijakan Privasi</a>
+								</li>
+								<li class="copyright-line">
+									<a href="<?=$host?>/web/responsible">Pertanggung Jawaban</a>
+								</li>
+								<li class="copyright-line">
+									<a href="<?=$host?>/web/tnc">Syarat &amp; Ketentuan</a>
+								</li>
+								<li class="copyright-line">
+									<a class="copyright-line" href="<?=$host?>/web/faq">FAQ</a>
+								</li>
+							</ul>
+						</div>
+
+					</div>
+        		</div>
+			</div>
+		</div>
+        
+		<div class="row bg-black">
+			<div class="col-12">
+
+				<div class="footer-copyright text-center">
+					<p>
+						Copyright ©
+							<script>document.write(new Date().getFullYear())</script>2023
+						PokkaWin.com. All Rights Reserved. +18
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
